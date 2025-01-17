82b06a48ee75aeca7c49f0a987fd432791ede382
chawan <chawandossantos7@gmail.com>
2025-01-17 14:16:34 -0300

    teste deploy

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-    input: '/path/to/main.jsx',
-    },
-  },
   plugins: [react()],
 })
