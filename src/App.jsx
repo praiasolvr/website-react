1120e9dcbb22e79d502e8fa8b60e838a91d33643
chawan <chawandossantos7@gmail.com>
2025-01-20 09:50:53 -0300

    colocando redirects no public para deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import RouterApp from './routes/RouterApp'
 import Header from './components/Header'
 
 
-// import { Link } from 'react-router-dom'
-
 import Footer from './components/Footer'
 
 import { BrowserRouter } from "react-router-dom";
 
 import '../src/assets/foto-internabus.jpg'
 
-// import MenuBoxModel from './models/MenuBoxModel/index.jsx'
 
 
 export default function App() {
