790a31b4e318768984a4a777347999971bb2d564
chawan <chawandossantos7@gmail.com>
2025-01-21 10:16:23 -0300

    Teste deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // build: {
+  //   outDir: 'dist',
+  // },
+
+  server: {
+    cors: {
+      // the origin you will be accessing via browser
+      origin: 'https://viacao-praiasol.netlify.app/',
+    },
+  },
   build: {
-    outDir: 'dist',
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      
+      input: './src/main.jsx',
+    },
   },
 })
