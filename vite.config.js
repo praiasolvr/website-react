467b92a537ab4c4b36669a3ec784163fa2fe5f88
chawan <chawandossantos7@gmail.com>
2025-01-21 11:37:09 -0300

    configurando base url

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   outDir: 'dist',
-  // },
-
-  server: {
-    cors: {
-      // the origin you will be accessing via browser
-      origin: 'https://viacao-praiasol.netlify.app/*',
-    },
-  },
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      
-      input: './src/main.jsx',
-    },
-  },
+  base: 'https://website-praia-sol.netlify.app', // ou '/nome-do-repositorio/' se for um subcaminho no GitHub Pages
 })
