f08f73ed040f40213150df94d668387123efe85c
chawan <chawandossantos7@gmail.com>
2025-01-21 11:45:58 -0300

    corrigindo config netlify

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://website-praia-sol.netlify.app', // ou '/nome-do-repositorio/' se for um subcaminho no GitHub Pages
+  base: '/', // ou '/nome-do-repositorio/' se for um subcaminho no GitHub Pages
 })
